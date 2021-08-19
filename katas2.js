aca9f5c119e08c54f4e1101902135942fcfc8372
allangratts <allangrattsdossantos@outlook.com>
2021-08-19 15:35:02 -0300

    adiconando as funções

--- katas2.js
+++ katas2.js
@@ -1,37 +1,56 @@
-// comece a criar a sua função add na linha abaixo
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
-
-
-// comece a criar a sua função multiply na linha abaixo
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
-
-
-// comece a criar a sua função power na linha abaixo
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
-
-
-// comece a criar a sua função factorial na linha abaixo
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
-
-
-/**
- * BONUS (aviso: o grau de dificuldade é bem maior !!!)
- */
-
-// crie a função fibonacci
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(fibonacci(7) === 13, 'A função fibonacci não está funcionando como esperado');
+function add (n,m) {
+    entrega = n+m;
+    return entrega;
+  }
+  
+  
+  
+  
+  function multiply (a,b) {
+  
+    let x = 0; 
+                         
+    for (let i = 0 ; i < b; i++) {
+                 
+      x = add (a,x)  
+      
+    }
+  
+    return x; 
+    
+  }
+  
+  
+  function power (x,n) {
+    
+    let z = 1;
+  
+    for (let i = 1; i <= n ; i++) {
+      
+      z = multiply (x,z);
+  
+    }
+  
+    return z;
+  }
+  
+  
+  
+  
+  function factorial (numero) {
+  
+    let n = 0;
+  
+    for (let i = numero; i = 1; i--) {
+      
+      n = multiply (numero, i);
+  
+    }
+  
+    return n;
+  
+  }
+  
+  
+  
+  
